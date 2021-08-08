7a4ac8bebb8c720f4110ddc0f00fc61041a02a87
Rutwik1 <83903631+Rutwik1@users.noreply.github.com>
2021-08-08 13:27:35 +0530

    Update Resume data in JSON format.js

--- Day 4/Resume data in JSON format.js
+++ Day 4/Resume data in JSON format.js
@@ -1,1 +1,35 @@
 
+let Resume = {
+    PersonalDetails: {
+        name: "Rutwik Kalbandhe",
+        age: 24,
+        DOB: "16/August/1996",
+        Gender: "Male",
+        Mob: 8605044820,
+        emailId: "rutkalbandhe@gmail.com",
+        Nationality: "Indian"
+    },
+    Education_Qualification: {
+        SSC: {
+            Percentage: 53,
+            Grade: 5.6,
+            School: "St. Peter's School",
+            Board: "CBSE"
+
+        },
+        HSC: {
+            Percentage: 62.61,
+            School: "GKHS",
+            Board: "State Board"
+        },
+        Graduation: {
+            College_Name: "GWCET",
+            Course: "B.E in EE",
+            Status: "Last Year"
+        },
+    },
+    Skills: ["Autocad", "Autocad Electrical"],
+    Hackathon: ["Guvi face Recognition Using AI"],
+    Interest: ["Gyming", "Travelling", "Phtography"],
+};
+console.log(Resume);
