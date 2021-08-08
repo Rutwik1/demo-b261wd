fa7182171e9d4ee0ee30e2e269620b09811ca42e
Rutwik1 <83903631+Rutwik1@users.noreply.github.com>
2021-08-08 14:37:58 +0530

    Update Technical Specifications.js

--- Day 5/Technical Specifications.js
+++ Day 5/Technical Specifications.js
@@ -1,1 +1,52 @@
 
+let technicalSpecifications = {
+    Dimensions: {
+        overAllLength: "3500 mm",
+        overAllWidth: "1600 mm",
+        overAllHeight: "1490 mm",
+        wheelBase: "2300 mm",
+        trackWidth: {
+            Front: "1405 mm",
+            Rear: "1400 mm"
+        },
+        minimumTumingRadius: "405 m",
+        minimumGroundClearance: "170 mm"
+    },
+    Capacities: {
+        seatingCapacity: "5 persons",
+        fuelTankCapacity: "35 litres"
+    },
+    Engine: {
+        Type: "KB-Series",
+        numberOfCylinders: 3,
+        numberOfValves: 12,
+        Capacity: "998 cc/cm3",
+        borexStroke: "73.0 x 79.5 mm",
+        compressionRatio: "10:1",
+        maximumPower: "67/6,200 PS/rpm",
+        maximumTorque: "90/3,500 Nm/rpm",
+        fuelDistribution: "Multipoint Injection"
+    },
+    Transmission: {
+        Type: "5-speed MT"
+    },
+    Chassis: {
+        Steering: "Rack & Pinion, Power assisted",
+        Brakes: {
+            Front: "Ventilated discs",
+            Rear: "Drums"
+        },
+        Suspension: {
+            Front: "MacPherson scrut & coil spring",
+            Rear: "Isolated trailing link & coil spring"
+        },
+        shockAbsorbers: "Gas filled",
+        Tyre: "155/80R 1 3",
+        tyreType: "Tubeless"
+    },
+    Weights: {
+        kerbWeight: "860-880 kg",
+        grossVehicleWeight: "1320 kg"
+    },
+};
+console.log(technicalSpecifications);  
